bb88da873b66d4c2d7f94992a32c2f5a760c7c31
Alex <67325048+AlexMalt-tech@users.noreply.github.com>
2023-01-28 23:18:20 +0300

    первый файл

---
+++ HW_1.py
@@ -0,0 +1,8 @@
+# strVar = input('Введите фразу: ')
+
+# print (strVar)
+
+num_1 = int(input('Введите первое число: '))
+num_2 = int(input('Введите второе число: '))
+
+print(f'Сумма {num_1} + {num_2} = {num_1 + num_2}')
