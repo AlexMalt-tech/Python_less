0e51b38b443437a4ea39e9d055623e5d07529954
Alex-tech <code.box@bk.ru>
2023-02-02 16:48:04 +0300

    решение задания 2 через строку

--- hw_1.3.py
+++ hw_1.3.py
@@ -6,6 +6,9 @@
 Введите число n: 3
 n + nn + nnn = 369
 """
-n = int(input('Введите число n: '))
 
-print(f'n + nn + nnn = {n}{n*2}{n*3}')
+n = input("Введите число n: ")
+temp_1 = n + n
+temp_2 = n + n + n
+n_sum = int(n) + int(temp_1) + int(temp_2) 
+print(f'n + nn + nnn = {n_sum}' )
